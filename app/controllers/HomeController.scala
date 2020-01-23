f4a7565a93b31858c5ec37ee5ba1be694ea2d2f2
Maftunbek <prince777_98@mail.ru>
2020-01-23 10:09:07 +0500

    Initial commit

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,27 @@
+package controllers
+
+import akka.util.Timeout
+import com.typesafe.scalalogging.LazyLogging
+import javax.inject._
+import org.webjars.play.WebJarsUtil
+import play.api.mvc._
+import views.html._
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration.DurationInt
+
+@Singleton
+class HomeController @Inject()(val controllerComponents: ControllerComponents,
+                               implicit val webJarsUtil: WebJarsUtil,
+                               indexTemplate: index
+                              )
+                              (implicit val ec: ExecutionContext)
+  extends BaseController with LazyLogging {
+
+  implicit val defaultTimeout: Timeout = Timeout(60.seconds)
+
+  def index = Action {
+    Ok(indexTemplate())
+  }
+
+}
